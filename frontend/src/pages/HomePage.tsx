51906b9e2b1000b9ae968dccbfc7b592564866a7
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-01-24 17:55:07 +0700

    setup fetch api

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,67 @@
+import { useEffect, useReducer } from "react";
+import { Product } from "../types/Product";
+import axios from "axios";
+import { ApiError } from "../types/ApiError";
+import { getError } from "../utils";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
+type State = {
+  products: Product[];
+  loading: boolean;
+  error: string;
+};
+
+type Action =
+  | { type: "FETCH_REQUEST" }
+  | {
+      type: "FETCH_SUCCESS";
+      payload: Product[];
+    }
+  | { type: "FETCH_FAIL"; payload: string };
+
+const initialState: State = {
+  products: [],
+  loading: true,
+  error: "",
+};
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const [{ loading, error, products }, dispatch] = useReducer<
+    React.Reducer<State, Action>
+  >(reducer, initialState);
 
-export default HomePage
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err as ApiError) });
+      }
+    };
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
+    <div>Home pape</div>
+  );
+};
+
+export default HomePage;
