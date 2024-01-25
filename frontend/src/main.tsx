2af3d69e610feedfd2a527d0df6d329da869d4b0
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-01-25 21:21:13 +0700

    remove axios and add reactQuery in HomePage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,9 +12,8 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import ProductPage from "./pages/ProductPage.tsx";
-import axios from "axios";
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:4000" : "/";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -25,10 +24,14 @@ const router = createBrowserRouter(
     </Route>
   )
 );
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
