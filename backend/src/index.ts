4f9518563cf9f5f2930e5f06186a25897d2f4dc4
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-01-25 21:58:20 +0700

    create api productDetails and useQueryProductDetails and PageProduct

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,9 +9,14 @@ app.use(
     origin: ['http://localhost:5173'],
   })
 )
+
 app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts)
 })
+app.get('/api/products/:slug', (req: Request, res: Response) => {
+  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
+})
+
 const PORT = 4000
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`)
