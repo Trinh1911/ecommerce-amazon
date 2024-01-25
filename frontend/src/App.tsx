ffc3264ccd030a6cb1db5cbb2cdf2db9b4b82f88
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-01-25 22:22:56 +0700

    context button light and dark

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useContext, useEffect } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
+import { Store } from "./Store";
 function App() {
+  const {
+    state: { mode },
+    dispatch,
+  } = useContext(Store)
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', mode)
+  }, [mode])
+
+  const switchModeHandler = () => {
+    dispatch({ type: 'SWITCH_MODE' })
+  }
   return (
     <div className="vh-100">
       <header>
@@ -9,6 +23,9 @@ function App() {
             <Navbar.Brand><Link to='/'>Amazon</Link></Navbar.Brand>
           </Container>
           <Nav>
+          <Button variant={mode} onClick={switchModeHandler}>
+              <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
+            </Button>
             <a href="/cart" className="nav-link">
               Cart
             </a>
