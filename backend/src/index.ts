f2a9df0047472f172a03a9cb073244d27c392e95
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-01-26 22:34:13 +0700

    connect mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,29 @@
 import cors from 'cors'
-import express, { Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { sampleProducts } from './data'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+dotenv.config()
 
-const app = express()
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/tsamazon'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
+  const app = express()
 app.use(
   cors({
     credentials: true,
     origin: ['http://localhost:5173'],
   })
 )
-
 app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts)
 })
