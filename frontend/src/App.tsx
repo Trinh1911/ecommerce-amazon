f487a863da5cc9b8f61d523b7549afba7fe29205
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-01-27 22:24:12 +0700

    add product to cart by redux comtermize

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { useContext, useEffect } from "react";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Badge, Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 import { Store } from "./Store";
 function App() {
   const {
-    state: { mode },
+    state: { mode, cart },
     dispatch,
   } = useContext(Store)
 
@@ -26,9 +26,14 @@ function App() {
           <Button variant={mode} onClick={switchModeHandler}>
               <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
             </Button>
-            <a href="/cart" className="nav-link">
+            <Link to="/cart" className="nav-link">
               Cart
-            </a>
+              {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}
+            </Link>
             <a href="/signin" className="nav-link">
               Sign In
             </a>
